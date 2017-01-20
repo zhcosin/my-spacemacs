b846a77456eb046cf0307585b768e7bae5329faf
zhcosin <zhcosin@gmail.com>
2017-01-20 23:29:02 +0800

    disable truncate-lines in org-mode hook.

--- init.el
+++ init.el
@@ -296,6 +296,9 @@ before packages are loaded. If you are unsure, you should try in setting them in
     '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
       ("org-cn"   . "http://elpa.emacs-china.org/org/")
       ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+
+(add-hook 'org-mode-hook 
+          (lambda () (setq truncate-lines nil))) 
   )
 
 (defun dotspacemacs/user-config ()
